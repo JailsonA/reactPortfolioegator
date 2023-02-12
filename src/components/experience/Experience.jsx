058910c4881a326e39365c1b2965d479d9e48b63
Jracker <maurocv18@gmail.com>
2023-02-12 02:48:31 +0000

    Experience Component JSX - Experience Component CSS - Experience Component Responsive Design - Services Component JSX - Services Component CSS - Services Component Responsive Design

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,110 @@
 import './experience.css';
-
+import {BsCheck2Circle} from 'react-icons/bs';
 function Experience() {
   return (
-    <section id='experience' >
-      
-      Experience
+    <section id='experience' className='experience_el' >
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience_container">
+        <div className='experience_frontend'>
+          <h3>Frontend Development</h3>
+          <div className="experience_content">
+            <article className='experience_details'>
+              <BsCheck2Circle className='experience_details-icon'/>
+              <div>
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <BsCheck2Circle className='experience_details-icon'/>
+              <div>
+              <h4>CSS</h4>
+              <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <BsCheck2Circle className='experience_details-icon'/>
+              <div>
+              <h4>JavaScript</h4>
+              <small className='text-light'>Intermediate</small>                
+              </div>
+            </article>
+            <article className='experience_details'>
+              <BsCheck2Circle className='experience_details-icon'/>
+              <div>
+                
+              <h4>Bootstrap</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <BsCheck2Circle className='experience_details-icon'/>
+              <div>
+                
+              <h4>Tailwind</h4>
+              <small className='text-light'>Intermediate</small>
+              </div>
+            </article>            
+            <article className='experience_details'>
+              <BsCheck2Circle className='experience_details-icon'/>
+              <div>
+                
+              <h4>React</h4>
+              <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* End of Frontend */}
+        <div className='experience_backend'>
+          <h3>Backend Development</h3>
+          <div className="experience_content">
+            <article className='experience_details'>
+              <BsCheck2Circle className='experience_details-icon'/>
+              <div>
+              <h4>MongoDB</h4>
+              <small className='text-light'>Learning</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <BsCheck2Circle className='experience_details-icon' />
+              <div>
+              <h4>PHP</h4>
+              <small className='text-light'>Basic</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <BsCheck2Circle className='experience_details-icon' />
+              <div>
+              <h4>Mysql</h4>
+              <small className='text-light'>Basic</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <BsCheck2Circle className='experience_details-icon'/>
+              <div>
+              <h4>Python</h4>
+              <small className='text-light'>Learning</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <BsCheck2Circle className='experience_details-icon' />
+              <div>
+              <h4>Laravel</h4>
+              <small className='text-light'>Intermediate</small>
+              </div>
+            </article>            
+            <article className='experience_details'>
+              <BsCheck2Circle className='experience_details-icon'/>
+              <div>
+              <h4>NodeJS</h4>
+              <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,97 @@
 import './services.css';
-
+import {BiCheck} from 'react-icons/bi'
 function Services() {
   return (
     <section id='services' >
-      
-      services
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services_container">
+        <article className='service'>
+          <div className="services_head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className='service_list'>
+            <li>
+              <BiCheck  className='service_list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck  className='service_list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck  className='service_list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck  className='service_list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck  className='service_list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF UI/UX */}
+        <article className='service'>
+          <div className="services_head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className='service_list'>
+            <li>
+              <BiCheck  className='service_list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck  className='service_list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck  className='service_list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck  className='service_list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck  className='service_list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF UI/UX */}
+        <article className='service'>
+          <div className="services_head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className='service_list'>
+            <li>
+              <BiCheck  className='service_list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck  className='service_list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck  className='service_list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck  className='service_list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck  className='service_list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF UI/UX */}
+      </div>
     </section>
   );
 }
